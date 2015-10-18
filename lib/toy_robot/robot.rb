9d633d03b7fbbcf28ad8982f24d87c077728af63
Victor Wibisono <vwibisono@dius.com.au>
2015-10-18 21:34:15 +1100

    Added robot left method.

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -12,5 +12,18 @@ module ToyRobot
       self.direction = dir.downcase.to_sym
     end
 
+    def left
+      case self.direction
+        when :north
+          self.direction = :west
+        when :west
+          self.direction = :south
+        when :south
+          self.direction = :east
+        when :east
+          self.direction = :north
+      end
+    end
+
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -26,4 +26,64 @@ describe Robot do
 
   end
 
+  describe '#left' do
+
+    context 'from north' do
+
+      before do
+        @robot = Robot.new
+        @robot.direction = :north
+        @robot.left
+      end
+
+      it 'should be west' do
+        expect(@robot.direction).to eq :west
+      end
+
+    end
+
+    context 'from west' do
+
+      before do
+        @robot = Robot.new
+        @robot.direction = :west
+        @robot.left
+      end
+
+      it 'should be south' do
+        expect(@robot.direction).to eq :south
+      end
+
+    end
+
+    context 'from south' do
+
+      before do
+        @robot = Robot.new
+        @robot.direction = :south
+        @robot.left
+      end
+
+      it 'should be east' do
+        expect(@robot.direction).to eq :east
+      end
+
+    end
+
+    context 'from east' do
+
+      before do
+        @robot = Robot.new
+        @robot.direction = :east
+        @robot.left
+      end
+
+      it 'should be north' do
+        expect(@robot.direction).to eq :north
+      end
+
+    end
+
+  end
+
 end

b0c65e37df4bb376edb2401c469a728625cd4d17
Victor Wibisono <vwibisono@dius.com.au>
2015-10-18 20:56:09 +1100

    Tidied up tests for robot.

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,24 +2,28 @@ require 'spec_helper'
 
 describe Robot do
 
-  context 'placing a robot' do
+  describe '#place' do
 
-    before do
-      @robot = Robot.new
-      @robot.place(1, 5, 'NORTH')
+    context 'after place' do
+
+      before do
+        @robot = Robot.new
+        @robot.place(1, 5, 'NORTH')
+      end
+
+      it 'robot should have the correct x coordinate' do
+        expect(@robot.x).to eq 1
+      end
+
+      it 'robot should have the correct y coordinate' do
+        expect(@robot.y).to eq 5
+      end
+
+      it 'robot should have the correct direction' do
+        expect(@robot.direction).to eq :north
+      end
     end
 
-    it 'should put robot in the correct x coordinate' do
-      expect(@robot.x).to eq 1
-    end
-
-    it 'should put robot in the correct y coordinate' do
-      expect(@robot.y).to eq 5
-    end
-
-    it 'should put robot in the correct direction' do
-      expect(@robot.direction).to eq :north
-    end
   end
 
 end

cf6b6f71e9e272180fd55aab49b75998b0562bf5
Victor Wibisono <vwibisono@dius.com.au>
2015-10-18 20:50:04 +1100

    Added the place command.

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,8 +1,15 @@
 module ToyRobot
   class Robot
 
-    def add(number)
-      return number + 1
+    # 3 robot attributes: x, y coordinates and direction
+    attr_accessor :x, :y, :direction
+
+    BOARD_SIZE = 5
+
+    def place(x, y, dir)
+      self.x = x
+      self.y = y
+      self.direction = dir.downcase.to_sym
     end
 
   end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,14 +2,23 @@ require 'spec_helper'
 
 describe Robot do
 
-  context 'add a number' do
+  context 'placing a robot' do
 
     before do
       @robot = Robot.new
+      @robot.place(1, 5, 'NORTH')
     end
 
-    it 'should return number plus one' do
-      expect(@robot.add(1)).to eq 2
+    it 'should put robot in the correct x coordinate' do
+      expect(@robot.x).to eq 1
+    end
+
+    it 'should put robot in the correct y coordinate' do
+      expect(@robot.y).to eq 5
+    end
+
+    it 'should put robot in the correct direction' do
+      expect(@robot.direction).to eq :north
     end
   end
 
